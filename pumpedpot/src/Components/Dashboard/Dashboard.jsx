98c28de701d0355b4e020888b98d531880e27215
Hemant Chaudhari <hemantchaudhari1587@gmail.com>
2023-03-31 02:15:10 +0530

    day3

---
+++ pumpedpot/src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+
+import {Box} from "@chakra-ui/react"
+import WithSubnavigation from "./Navbar"
+
+
+
+ export default function Dashboard(){
+    return(
+        <Box>
+        <WithSubnavigation/>
+        </Box>
+    )
+
+}
